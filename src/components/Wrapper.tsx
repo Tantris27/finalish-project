1721ca218746590e0fe3d8c45d1bbc6f3c133665
Tristan <tantris.kantner@gmail.com>
2021-06-24 11:41:56 +0200

    Set up new Branch

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/core';
 
 interface WrapperProps {}
-
+// Comment for branchCreation
 export const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   return <Box>{children}</Box>;
 };
